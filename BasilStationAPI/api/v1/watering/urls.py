66d9749ea0447ae049dad9216052100f1a436bd2
Ole Rößler <ole.nelson.r@gmail.com>
2023-10-01 22:05:19 +0200

    added watering routes the /api/v1/watering

--- BasilStationAPI/api/v1/watering/urls.py
+++ BasilStationAPI/api/v1/watering/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path("", views.get_all_waterings),
     path("<int:watering_id>", views.get_watering),
-    path("watering/",include("api.v1.watering.urls")),
-
 ]
--- BasilStationAPI/api/v1/watering/views.py
+++ BasilStationAPI/api/v1/watering/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.decorators import api_view
 from base.models import Watered
-from .watering.serializers import WateredSerializer
+from .serializers import WateredSerializer
 
 # Create your views here.
 
