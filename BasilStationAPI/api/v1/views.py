39376ed48778e03c593a68f04ef8144b987683c2
Ole Rößler <ole.nelson.r@gmail.com>
2023-10-01 21:56:21 +0200

    implemented id doesnt exist

--- BasilStationAPI/api/v1/views.py
+++ BasilStationAPI/api/v1/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
+from django.core.exceptions import ObjectDoesNotExist
+
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.decorators import api_view
@@ -22,8 +25,10 @@ def get_all_waterings(request):
 #get the last three waterings
 @api_view(['GET'])
 def get_watering(request, watering_id):
-    #TODO what if id doesnt exist?
-    waterings = Watered.objects.get(pk=watering_id) 
-    serilizer = WateredSerializer(waterings, many=False)
-    
+    try:
+        waterings = Watered.objects.get(pk=watering_id) 
+        serilizer = WateredSerializer(waterings, many=False)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound("Watering with id: {} doesnt exist".format(watering_id), )
+
     return Response(serilizer.data)
