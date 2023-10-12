2be7058b0cd32d0678c028af0e55fc8766ee85aa
Ole Rößler <ole.nelson.r@gmail.com>
2023-10-12 16:48:40 +0200

    Changed the pump so u only need to call pump

--- BasilGPIO/ActivatePump.py
+++ BasilGPIO/ActivatePump.py
@@ -2,16 +2,16 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 
-def setup():
+def __setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(16,GPIO.OUT)
 
 def pump(duration):
-    setup()
+    __setup()
     GPIO.output(17, True)
     sleep(3)
     GPIO.output(17, False)
-    cleanup()
+    __cleanup()
 
-def cleanup():
+def __cleanup():
     GPIO.cleanup()
