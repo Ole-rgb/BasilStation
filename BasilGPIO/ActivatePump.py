63961266cec6ff24ed41ed795f5805ccfb74b881
Ole Rößler <ole.nelson.r@gmail.com>
2023-10-12 16:47:45 +0200

    Changed

--- BasilGPIO/ActivatePump.py
+++ BasilGPIO/ActivatePump.py
@@ -7,9 +7,11 @@ def setup():
     GPIO.setup(16,GPIO.OUT)
 
 def pump(duration):
+    setup()
     GPIO.output(17, True)
     sleep(3)
     GPIO.output(17, False)
+    cleanup()
 
 def cleanup():
     GPIO.cleanup()
